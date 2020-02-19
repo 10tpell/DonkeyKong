33004521d1390483abe49f5b8d1fd3a2e189e1e0
Travis Pell <tpell1@sheffield.ac.uk>
2020-02-19 16:44:22 +0000

    Added jumping and basic physics

--- DonkeyKong/GameScene.swift
+++ DonkeyKong/GameScene.swift
@@ -15,64 +15,43 @@ class GameScene: SKScene {
     var graphs = [String : GKGraph]()
     
     private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var marioSprite : SKSpriteNode?
     
     override func sceneDidLoad() {
 
         self.lastUpdateTime = 0
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        marioSprite = SKSpriteNode(imageNamed: "mario.png")
+        marioSprite?.physicsBody = SKPhysicsBody(texture: (marioSprite?.texture)!, size: (marioSprite?.texture!.size())!)
+        marioSprite?.physicsBody?.usesPreciseCollisionDetection = true
+        self.addChild((marioSprite)!)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        let ground = SKShapeNode()
+        ground.path = UIBezierPath(roundedRect: CGRect(x: frame.minX, y: 0, width: frame.maxX * 2, height: 20), cornerRadius: 1).cgPath
+        ground.position = CGPoint(x: frame.midX, y: frame.minY)
+        ground.fillColor = UIColor.green
+        ground.lineWidth = 5
+        ground.physicsBody = SKPhysicsBody(edgeChainFrom: ground.path!)
+        ground.physicsBody?.restitution = 0.2
+        ground.physicsBody?.isDynamic = false
+        self.addChild(ground)
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+        marioSprite?.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 500))
+        print("Touch down")
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
-        
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {        
         for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
